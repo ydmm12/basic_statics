49b99aec8874f970c068a3987cd334e10f817863
Yamil Mendoza <yamil_david@hotmail.com>
2023-12-01 17:00:25 -0600

    Fixing comments

--- models/data_capture.py
+++ models/data_capture.py
@@ -2,11 +2,22 @@ from models.stats import Stats
 from exceptions.exceptions import check_integers
 
 class DataCapture:
+    """Class to get the data and send it to be processed, when it's initialized create a list with all the values at zero."""
     data = [0] * 1000
     
     @check_integers
-    def add(self, number):
+    def add(self, number: int):
+        """Function to get a new value and add it to the position in the list.
+
+        Args:
+            number (int): Value to add.
+        """
         self.data[number] += 1
         
-    def build_stats(self):
+    def build_stats(self) -> Stats:
+        """Function to send all data to be processed all the data to create the stats.
+
+        Returns:
+            Stats: An object with all the processed data.
+        """
         return Stats(self.data)
--- models/stats.py
+++ models/stats.py
@@ -1,20 +1,51 @@
 from exceptions.exceptions import check_integers
 
 class Stats:
-    def __init__(self, data):
+    """Class to get the statistics of the data received."""
+    def __init__(self, data: list):
+        """When the object is initialized, it fills all the values with the sum of the previous numbers.
+
+        Args:
+            data (list): The data with the numeric values per position.
+        """
         for number in range(1,len(data)):
             data[number]+=data[number-1]
         self.data = data
             
     @check_integers
-    def less(self, number):
+    def less(self, number: int) -> int:
+        """Function to get all numbers before the value.
+
+        Args:
+            number (int): Numeric value to get all the numbers before, the number must always be between 1 and 999.
+
+        Returns:
+            int: Number of values.
+        """
         return self.data[number-1]
             
     @check_integers
-    def greater(self, number):
+    def greater(self, number:int) -> int:
+        """Function to get all numbers after the value.
+
+        Args:
+            number (int): Numeric value to get all the numbers after, the number must always be between 1 and 999.
+
+        Returns:
+            int: Number of values.
+        """
         return self.data[-1] - self.data[number]
             
     @check_integers
-    def between(self, begin, finish):
+    def between(self, begin:int, finish:int) -> int:
+        """Function to get all numbers between two values with these values included.
+
+        Args:
+            begin (int): Numeric value with the start of the count, the number must always be between 1 and 999 and less than the finish value.
+            finish (int): Numeric value with the end of the count, the number must always be between 1 and 999 and greater than the begin value.
+
+        Returns:
+            int: Number of values.
+        """
         dif = self.data[begin-1] if begin != 0 else 0
         return self.data[finish] - dif
